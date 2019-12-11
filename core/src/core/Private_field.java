3f96f3037a6157f7fb5e23d159450024969a5efe
Ajay Mirgal <ajay.mirgal@hsmumsz.local>
2019-12-11 16:34:45 +0530

    core package

---
+++ core/src/core/Private_field.java
@@ -0,0 +1,28 @@
+package core;
+
+//Imports //
+
+
+public class Private_field {
+	
+	private int a;
+	private scanner;
+	//
+	private void getUseInput(){
+		Scanner scanner = new Scanner(System.in)
+		a = 10;
+	}
+	
+	
+	
+	
+	
+	
+	
+	//
+	public static void main(String[] args) {
+		
+		// 
+	}
+
+}
